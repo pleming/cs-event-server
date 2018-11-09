793f9bd1c78f8a800cd78262c59ebee9884306c3
Hojin Choe <hojin.choe.dev@gmail.com>
2018-11-09 09:29:40 +0900

    feat: Add api modules to get CS information

---
+++ api/loadCSInfo.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var router = express.Router();
+
+router.get("/", function (req, res, next) {
+    req.sql("SELECT * FROM cs_type", function (err, rows) {
+        if (err) {
+            res.json({status: false, code: 1, contents: err});
+            return;
+        }
+
+        res.json({status: true, code: 1, contents: rows});
+    });
+});
+
+module.exports = router;
---
+++ api/loadProductInfo.js
@@ -0,0 +1,38 @@
+var express = require("express");
+var router = express.Router();
+
+router.get("/", function (req, res, next) {
+    var param = req.query;
+    var query = "SELECT " +
+        "CS.name AS csName, " +
+        "ET.name AS eventName, " +
+        "CSP.name AS productName, " +
+        "CSP.price AS price " +
+        "FROM cs_product AS CSP " +
+        "INNER JOIN cs_type AS CS " +
+        "ON CSP.cs_type = CS.id " +
+        "INNER JOIN event_type AS ET " +
+        "ON CSP.event_type = ET.id " +
+        "WHERE CSP.cs_type = ${csType} AND CSP.event_type = ${eventType}";
+
+    if (typeof param.csType === "undefined")
+        query = query.replace("CSP.cs_type = ${csType}", "1=1");
+    else
+        query = query.replace("${csType}", param.csType);
+
+    if (typeof param.eventType === "undefined")
+        query = query.replace("CSP.event_type = ${eventType}", "1=1");
+    else
+        query = query.replace("${eventType}", param.eventType);
+
+    req.sql(query, function (err, rows) {
+        if (err) {
+            res.json({status: false, code: 1, contents: err});
+            return;
+        }
+
+        res.json({status: true, code: 1, contents: rows});
+    });
+});
+
+module.exports = router;
